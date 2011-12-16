98d74160705a638c11f6c217b19a99f166824a50
Andrew Lee <starsthatfell@gmail.com>
2011-12-16 01:59:23 -0600

    added fns

---
+++ snippets.js
@@ -0,0 +1,29 @@
+/*
+add slashes to regular expression meta characters in strings so that they can be escaped
+used with dynamically generated regular expressions
+*/
+function escapeRegExMeta(str){
+    var meta = '\\,^,[,.,$,{,*,(,+,),|,?,<,>'.split(',');
+
+    for(var i=0, l=meta.length; i<l; i++){
+        str = str.replace(meta[i], '\\' + meta[i]);
+    }
+
+    return str;
+}
+
+
+/*
+regular expression to retrieve a value from a serialized string based on a key
+example: 'key1=value1; key2=value2; key3=value3'
+sample use: cookies
+*/
+var reg = new RegExp('(^'+key+'|; '+key+')=([^;]+)');
+
+
+/*
+checks if a variable is a true array
+*/
+function isArray(a){
+	return Object.prototype.toString.call(a) === '[object Array]';
+}
